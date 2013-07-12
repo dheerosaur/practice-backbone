fdc93029a2080a079c3856ec21b3042f4aa405ab
Dheeraj Sayala <dheerosaur@gmail.com>
2013-07-12 13:53:22 +0530

    Add snippets to load jQuery, Underscore, Backbone

---
+++ snippets.js
@@ -0,0 +1,11 @@
+var tag = document.createElement('script');
+tag.src = "http://code.jquery.com/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(tag);
+
+var tag = document.createElement('script');
+tag.src = "http://documentcloud.github.com/underscore/underscore.js";
+document.getElementsByTagName('head')[0].appendChild(tag);
+
+var tag = document.createElement('script');
+tag.src = "http://documentcloud.github.com/backbone/backbone.js";
+document.getElementsByTagName('head')[0].appendChild(tag);
