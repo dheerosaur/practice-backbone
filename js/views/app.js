a25bfd10e1bf52fdd61fd1e8cbf6ec7a462ddca5
Dheeraj Sayala <dheerosaur@gmail.com>
2013-07-15 22:17:57 +0530

    Setup app and the main AppView

---
+++ js/views/app.js
@@ -0,0 +1,14 @@
+var app = app || {};
+
+app.AppView = Backbone.View.extend({
+  // This is the element which is used to create this.$el
+  // Generally, render method injects data into this
+  el: '#todoapp',
+
+  initialize: function () {
+  },
+
+  render: function () {
+  }
+
+});
